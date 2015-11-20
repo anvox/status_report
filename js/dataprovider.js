b27861fc306f15acc02a70cc0621d9521358f0b5
An Vo Nguyen Thien <an.vo@codebox.vn>
2015-11-20 12:05:11 +0700

    Fix json format

--- js/dataprovider.js
+++ js/dataprovider.js
@@ -1,35 +1,35 @@
 ESDataProvider = function(){
   this.get_by_hour = function(date, success_handler, failure_handler){
-    body_template = {
-      "query" : {
-        "bool": {
-          "must":[
+    body = {
+      query: {
+        bool: {
+          must:[
             {
-              "query_string": {
-                "query": 'type:"rails" AND tags:(NOT _jsonparsefailure) AND user_id:>0'
+              query_string: {
+                query: 'type:"rails" AND tags:(NOT _jsonparsefailure) AND user_id:>0'
               }
             },
             {
-              "range": {
-                "@timestamp" : {
-                    "gte" : "2015-10-1",
-                    "lt" :  "2015-11-1"
+              range: {
+                @timestamp : {
+                    gte : "2015-10-1",
+                    lt :  "2015-11-1"
                 }
               }
             }
           ]
         }
       },
-      "aggs": {
-        "0": {
-          "date_histogram": {
-            "field": "@timestamp",
-            "interval": "1h"
+      aggs: {
+        0: {
+          date_histogram: {
+            field: "@timestamp",
+            interval: "1h"
           },
-          "aggs": {
-            "distinct_user_id" : {
-                "cardinality" : {
-                  "field" : "user_id"
+          aggs: {
+            distinct_user_id: {
+                cardinality: {
+                  field: "user_id"
                 }
               }
             }
@@ -41,7 +41,7 @@ ESDataProvider = function(){
     query = 'localhost/logstash-2015.11.16/_search';
     var request = $.ajax({
       url: query,
-      method: "GET",
+      method: "POST",
       data: body,
       dataType: "json"
     });
